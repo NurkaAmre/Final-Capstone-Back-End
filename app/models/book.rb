92b8ae8186a3ed690f9c054a3b4068145759d06b
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-03-03 20:18:03 +0530

    fix rubocop

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,15 +1,15 @@
 class Book < ApplicationRecord
-    has_many :reservations
-    has_many :users, through: :reservations
+  has_many :reservations
+  has_many :users, through: :reservations
 
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :book_cover_image, presence: true
-    validates :genre, presence: true
-    validates :publisher, presence: true
-    validates :date_of_publication, presence: true
-    validates :pages, presence: true
-    validates :language, presence: true
-    validates :isbn, presence: true
-    validates :price, presence: true
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :book_cover_image, presence: true
+  validates :genre, presence: true
+  validates :publisher, presence: true
+  validates :date_of_publication, presence: true
+  validates :pages, presence: true
+  validates :language, presence: true
+  validates :isbn, presence: true
+  validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :reservations
-    has_many :books, through: :reservations
-    
-    validates :user_name, length: { in: 5..30 }
+  has_many :reservations
+  has_many :books, through: :reservations
+
+  validates :user_name, length: { in: 5..30 }
 end
