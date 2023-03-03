b0c65110815ae9ec80786a3ec94ceed869c4086c
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-03-03 19:29:39 +0530

    set up database for books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,14 @@
 class Book < ApplicationRecord
+    has_many :reservations
+    has_many :users, through: :reservations
+
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :genre, presence: true
+    validates :publisher, presence: true
+    validates :date_of_publication, presence: true
+    validates :pages, presence: true
+    validates :language, presence: true
+    validates :isbn, presence: true
+    validates :price, presence: true
 end
--- db/migrate/20230303132010_create_books.rb
+++ db/migrate/20230303132010_create_books.rb
@@ -1,6 +1,15 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.text :genre
+      t.string :publisher
+      t.date :date_of_publication
+      t.integer :pages
+      t.string :language
+      t.string :isbn
+      t.integer :price
 
       t.timestamps
     end
