61d6e83aceb5ab5d4a62f0d63cb45cf5a558042f
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-03-03 19:13:54 +0530

    set up database for reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,8 @@
 class Reservation < ApplicationRecord
+    belongs_to :user, class_name: 'User'
+    belongs_to :book, class_name: 'Book'
+  
+    validates :city, presence: true
+    validates :date_of_booking, presence: true
+    validates :date_of_delivery, presence: true  
 end
--- db/migrate/20230303131934_create_reservations.rb
+++ db/migrate/20230303131934_create_reservations.rb
@@ -1,6 +1,11 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
+      t.string :city
+      t.date :date_of_booking
+      t.date :date_of_delivery
+      t.references :user, null: false, foreign_key: true, index: true
+      t.references :book, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
