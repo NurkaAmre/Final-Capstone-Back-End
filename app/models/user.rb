00fb8fc163bd0cb79c2608ed9cbc33470d364368
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-03-03 19:04:22 +0530

    set up database for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :registrations
+    has_many :books, through: :registrations
+    
+    validates :user_name, length: { in: 5..30 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(user_name: "Joseph")
+User.create(user_name: "Nurka")
+User.create(user_name: "Omar")
+User.create(user_name: "Tanusri")
+
