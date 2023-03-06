7fde088f2bde1c87f6dab25efa584750ec17881a
NurkaAmre <nurkerey30@gmail.com>
2023-03-06 18:11:48 +0600

    Setup users routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :reservations
   has_many :books, through: :reservations
 
   validates :user_name, length: { in: 5..30 }
+
+  def jwt_payload
+    super
+  end
 end
