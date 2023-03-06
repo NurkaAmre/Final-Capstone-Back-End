bd1f47dab6f5cf3f09aa2cf421c231cbd30328df
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-03-06 20:11:30 +0530

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :reservations, only: [:index, :destroy]
-      resources :bookss, only: [:index, :show, :new, :create, :destroy] do
+      resources :books, only: [:index, :show, :new, :create, :destroy] do
         resources :reservations , only: [:create]
       end
     end
