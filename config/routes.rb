d3a0b8af5de4f24c4876ff0e20ed6dbf89b4aeaf
NurkaAmre <nurkerey30@gmail.com>
2023-03-08 20:59:51 +0600

    Our work is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
-namespace :api do 
-  namespace :v1 do
-    resources :users , only: [:create]
-    resources :users, only: [:show], param: :user_name
-    resources :cars, only: [:create, :destroy]
-    resources :cars, only: [:index, :show]
-    resources :users, only: [:show] do
-      resources :reservations, only: [:create, :index]
+Rails.application.routes.draw do
+  
+  namespace :api do 
+    namespace :v1 do
+      resources :users , only: [:create]
+      resources :users, only: [:show], param: :user_name
+      resources :books, only: [:create, :destroy]
+      resources :books, only: [:index, :show]
+      resources :users, only: [:show] do
+        resources :reservations, only: [:create, :index]
 
+      end
     end
   end
 end
