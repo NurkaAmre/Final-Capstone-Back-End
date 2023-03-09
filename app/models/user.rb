31a97b96285d9dea612c064c9c3a9afef04af295
NurkaAmre <nurkerey30@gmail.com>
2023-03-09 16:56:12 +0600

    Change routes, user.rb, deleted migration file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :user_name, presence: true
   # Associations
   has_many :reservations
   has_many :books, through: :reservations
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_06_090554) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,8 +42,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_090554) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "reservations", "books"
