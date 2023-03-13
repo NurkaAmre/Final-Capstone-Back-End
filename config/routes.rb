0a4d8fe7f942e7f6ac16295adc0240b859bbabe0
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-03-13 16:34:01 +0530

    Update routes and usercontroller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api do 
     namespace :v1 do
+      resources :users
       resources :users , only: [:create]
       resources :users, only: [:show], param: :user_name
       resources :books, only: [:create, :destroy]
